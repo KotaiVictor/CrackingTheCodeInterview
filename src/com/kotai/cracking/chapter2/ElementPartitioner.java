7b649b094d142142e942b50924fa8544f181f5d1
Victor Kotai <kotai.viktor@gmail.com>
2015-07-04 12:00:06 +0100

    Linked list partitioner

---
+++ src/com/kotai/cracking/chapter2/ElementPartitioner.java
@@ -0,0 +1,42 @@
+package com.kotai.cracking.chapter2;
+
+import com.kotai.cracking.samples.TestNode;
+
+/*
+ * Write code to partition a linked list around a value x, such that all nodes less than x come
+ * before all nodes greater than or equal to x.
+ */
+public class ElementPartitioner {
+
+  public <T extends Comparable<T>> TestNode<T> partition(TestNode<T> head, T value) {
+    TestNode<T> leftHead = null;
+    TestNode<T> leftCurrent = null;
+    TestNode<T> rightHead = null;
+    TestNode<T> rightCurrent = null;
+
+    while (head != null) {
+      if (value.compareTo(head.data) > 0) {
+        if (leftHead == null) {
+          leftCurrent = leftHead = new TestNode<T>(head.data);
+        } else {
+          leftCurrent.next = new TestNode<T>(head.data);
+          leftCurrent = leftCurrent.next;
+        }
+      } else {
+        if (rightHead == null) {
+          rightCurrent = rightHead = new TestNode<T>(head.data);
+        } else {
+          rightCurrent.next = new TestNode<T>(head.data);
+          rightCurrent = rightCurrent.next;
+        }
+      }
+      head = head.next;
+    }
+
+    if (leftHead != null) {
+      leftCurrent.next = rightHead;
+      return leftHead;
+    }
+    return rightHead;
+  }
+}
---
+++ test/com/kotai/cracking/chapter2/test/ElementPartitionerTest.java
@@ -0,0 +1,73 @@
+package com.kotai.cracking.chapter2.test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.kotai.cracking.chapter2.ElementPartitioner;
+import com.kotai.cracking.samples.TestNode;
+
+public class ElementPartitionerTest {
+
+  private ElementPartitioner partitioner = new ElementPartitioner();
+
+  @Test
+  public void list_case_value_in_list() {
+    TestNode<Integer> head = new TestNode<>(14);
+    head.appendToTail(13);
+    head.appendToTail(12);
+    head.appendToTail(11);
+    head.appendToTail(10);
+
+    List<Integer> expectedList = new ArrayList<Integer>();
+    expectedList.add(11);
+    expectedList.add(10);
+    expectedList.add(14);
+    expectedList.add(13);
+    expectedList.add(12);
+
+    assertThat(partitioner.partition(head, 12).toList(), equalTo(expectedList));
+  }
+
+  @Test
+  public void list_case_value_less_than_list() {
+    TestNode<Integer> head = new TestNode<Integer>(10);
+    head.appendToTail(11);
+    head.appendToTail(12);
+    head.appendToTail(13);
+
+    assertThat(partitioner.partition(head, 9).toList(), equalTo(head.toList()));
+  }
+
+  @Test
+  public void list_case_value_greater_than_list() {
+    TestNode<Integer> head = new TestNode<Integer>(10);
+    head.appendToTail(11);
+    head.appendToTail(12);
+    head.appendToTail(13);
+
+    assertThat(partitioner.partition(head, 14).toList(), equalTo(head.toList()));
+  }
+
+  @Test
+  public void empty_list_case() {
+    assertThat(partitioner.partition(null, 10), equalTo(null));
+  }
+
+  @Test
+  public void one_element_case_value_greater_than_whole_list() {
+    TestNode<Integer> head = new TestNode<Integer>(10);
+
+    assertThat(partitioner.partition(head, 11).toList(), equalTo(head.toList()));
+  }
+
+  @Test
+  public void one_element_case_value_less_than_list() {
+    TestNode<Integer> head = new TestNode<Integer>(10);
+    assertThat(partitioner.partition(head, 8).toList(), equalTo(head.toList()));
+  }
+}
