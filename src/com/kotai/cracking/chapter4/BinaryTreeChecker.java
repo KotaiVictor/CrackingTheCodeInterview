539206e2c568cbbac77f800dfde1bec530a75d14
Victor Kotai <kotai.viktor@gmail.com>
2015-07-17 22:39:28 +0100

    fixing BST checker, adding implementation that uses in order traversal

--- src/com/kotai/cracking/chapter4/BinaryTreeChecker.java
+++ src/com/kotai/cracking/chapter4/BinaryTreeChecker.java
@@ -1,22 +1,50 @@
 package com.kotai.cracking.chapter4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.kotai.cracking.samples.binarytree.BinaryTreeNode;
 
 public class BinaryTreeChecker {
-
   public <T extends Comparable<T>> boolean isBinarySearchTree(BinaryTreeNode<T> root) {
+    List<BinaryTreeNode<T>> inOrderList = inOrderTraversal(root);
+    BinaryTreeNode<T> previous = null;
+    for (BinaryTreeNode<T> current : inOrderList) {
+      if (previous != null && current.getData().compareTo(previous.getData()) <= 0)
+        return false;
+      previous = current;
+    }
+    return true;
+  }
+
+  /**
+   * So ineficient
+   */
+  private <T extends Comparable<T>> List<BinaryTreeNode<T>> inOrderTraversal(
+      BinaryTreeNode<T> root) {
+    ArrayList<BinaryTreeNode<T>> list = new ArrayList<>();
+    if (root == null)
+      return list;
+    list.addAll(inOrderTraversal(root.getLeft()));
+    list.add(root);
+    list.addAll(inOrderTraversal(root.getRight()));
+    return list;
+  }
+
+  public boolean isBST(BinaryTreeNode<Integer> root) {
+    return isBinarySearchTree(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+
+  private boolean isBinarySearchTree(BinaryTreeNode<Integer> root, Integer min, Integer max) {
     if (root == null)
       return true;
 
-    boolean isBinaryTreeLeft = true;
-    if (root.getLeft() != null)
-      isBinaryTreeLeft = isBinarySearchTree(root.getLeft())
-          && root.getLeft().getData().compareTo(root.getData()) <= 0;
+    if (root.getData() <= min || root.getData() > max)
+      return false;
 
-    boolean isBinaryTreeRight = true;
-    if (root.getRight() != null)
-      isBinaryTreeRight = isBinarySearchTree(root.getRight())
-          && root.getRight().getData().compareTo(root.getData()) >= 0;
+    boolean isBinaryTreeLeft = isBinarySearchTree(root.getLeft(), min, root.getData());
+
+    boolean isBinaryTreeRight = isBinarySearchTree(root.getRight(), root.getData(), max);
 
     return isBinaryTreeLeft && isBinaryTreeRight;
   }
--- test/com/kotai/cracking/chapter4/test/BinaryTreeCheckerTest.java
+++ test/com/kotai/cracking/chapter4/test/BinaryTreeCheckerTest.java
@@ -13,6 +13,12 @@ public class BinaryTreeCheckerTest {
 
   private BinaryTreeChecker checker = new BinaryTreeChecker();
 
+  private boolean doCheck(BinaryTreeNode<Integer> root, boolean useInOrder) {
+    if (useInOrder)
+      return checker.isBinarySearchTree(root);
+    return checker.isBST(root);
+  }
+
   @Test
   public void increasing_sequence_of_5_values_is_a_binary_search_tree() {
     BinaryTreeNode<Integer> root = new BinaryTreeNode<>(10);
@@ -23,8 +29,10 @@ public class BinaryTreeCheckerTest {
     root.getRight().setLeft(createNode(12));
     root.getRight().setRight(createNode(17));
 
-    assertThat(checker.isBinarySearchTree(root), equalTo(true));
+    assertThat(doCheck(root, true), equalTo(true));
+    assertThat(doCheck(root, false), equalTo(true));
   }
+
 
   @Test
   public void decreasing_sequence_of_5_values_is_not_a_binary_tree() {
@@ -36,6 +44,18 @@ public class BinaryTreeCheckerTest {
     root.getRight().setLeft(createNode(12));
     root.getRight().setRight(createNode(14));
 
-    assertThat(checker.isBinarySearchTree(root), equalTo(false));
+    assertThat(doCheck(root, true), equalTo(false));
+    assertThat(doCheck(root, false), equalTo(false));
+  }
+
+  @Test
+  public void inner_node_larger_than_root_sequence_is_not_a_binary_tree() {
+    BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(20);
+    root.setLeft(createNode(15));
+    root.setRight(createNode(30));
+    root.getLeft().setRight(createNode(25));
+
+    assertThat(doCheck(root, true), equalTo(false));
+    assertThat(doCheck(root, false), equalTo(false));
   }
 }
